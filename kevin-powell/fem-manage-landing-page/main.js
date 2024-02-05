f3d884d0ed17bd3b07ffd6085b7acdf3d99fc225
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-02-05 10:25:03 -0300

    Navigation desktop and mobile

---
+++ kevin-powell/fem-manage-landing-page/main.js
@@ -0,0 +1,10 @@
+const primaryHeader = document.querySelector(".primary-header")
+const navToggle = document.querySelector(".mobile-nav-toggle")
+const primaryNav = document.querySelector(".primary-navigation")
+
+navToggle.addEventListener("click", () => {
+    primaryNav.toggleAttribute("data-visible")
+    primaryHeader.toggleAttribute("data-overlay")
+    const isNavVisible = primaryNav.hasAttribute("data-visible")
+    navToggle.setAttribute("aria-expanded", isNavVisible)
+})
