5a4c08052aa732e6b833e8fed80ef17212183f7f
pedrofrohmut <pedrofrohmut@gmail.com>
2024-06-18 15:39:57 -0300

    TraversyMedia admin dashboard - Initial with navbar

---
+++ traversy-media/admin-dashboard/components/navbar.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link"
+import Image from "next/image"
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuItem
+} from "@/components/ui/dropdown-menu"
+
+import logoImage from "../img/logo.png"
+
+const Navbar = () => {
+    return (
+        <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+            <Link href="/">
+                <Image src={logoImage} alt="TraversyPress" width={40} />
+            </Link>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback className="text-black">BT</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+
+                    <DropdownMenuSeparator />
+
+                    <DropdownMenuItem>
+                        <Link href="/profile">Profile</Link>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem>
+                        <Link href="/auth">Sign Out</Link>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    )
+}
+
+export default Navbar
