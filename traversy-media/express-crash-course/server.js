b2c8c8a01466b5482c568aefcec47b939bc09aec
pedrofrohmut <pedrofrohmut@gmail.com>
2024-07-10 11:05:42 -0300

    Minimal static server with express js

--- traversy-media/express-crash-course/server.js
+++ traversy-media/express-crash-course/server.js
@@ -3,17 +3,20 @@ const path = require("path")
 
 const app = express()
 
-app.get("/", (req, res) => {
-    //res.send({ message: "Hello, World!!!" })
-    const indexFilePath = path.join(__dirname, "public", "index.html")
-    res.sendFile(indexFilePath)
-})
+// Setup static folder
+app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/about", (req, res) => {
-    //res.send({ message: "About the world" })
-    const aboutFilePath = path.join(__dirname, "public", "about.html")
-    res.sendFile(aboutFilePath)
-})
+//app.get("/", (req, res) => {
+//    //res.send({ message: "Hello, World!!!" })
+//    const indexFilePath = path.join(__dirname, "public", "index.html")
+//    res.sendFile(indexFilePath)
+//})
+//
+//app.get("/about", (req, res) => {
+//    //res.send({ message: "About the world" })
+//    const aboutFilePath = path.join(__dirname, "public", "about.html")
+//    res.sendFile(aboutFilePath)
+//})
 
 const PORT = process.env.PORT || 8000
 
